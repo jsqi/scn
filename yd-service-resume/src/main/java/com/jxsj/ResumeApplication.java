97ac66130f7c2c5bd48f8864f2e1e8f34d37b940
豆浆 <13673180154@163.com>
2022-02-08 23:03:58 +0800

    添加配置中心

--- yd-service-resume/src/main/java/com/jxsj/ResumeApplication.java
+++ yd-service-resume/src/main/java/com/jxsj/ResumeApplication.java
@@ -4,12 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EntityScan("com.jxsj.pojo")
-@EnableEurekaClient
 @EnableDiscoveryClient
+@RefreshScope
 public class ResumeApplication {
 
     public static void main(String[] args) {
