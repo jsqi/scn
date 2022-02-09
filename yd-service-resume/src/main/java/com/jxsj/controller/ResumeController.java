a8e5bcc103811a96fd27c090f5d770a4d63637c1
豆浆 <>
2022-02-09 11:40:59 +0800

    集成消息总线

--- yd-service-resume/src/main/java/com/jxsj/controller/ResumeController.java
+++ yd-service-resume/src/main/java/com/jxsj/controller/ResumeController.java
@@ -3,6 +3,7 @@ package com.jxsj.controller;
 import com.jxsj.service.ResumeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,15 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/resume")
+@RefreshScope
 public class ResumeController {
     @Value("${server.port}")
     private Integer port;
+    @Value("${test.name}")
+    private String name;
 
     @Autowired
     private ResumeService resumeService;
 
     @GetMapping("/openstate/{userId}")
     public Integer findDefaultResumeState(@PathVariable Long userId) {
+        System.out.println(name);
         return port;
         // return resumeService.findDefaultResumeByUserId(userId).getIsOpenResume();
     }
